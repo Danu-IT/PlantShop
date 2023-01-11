e074c5c58bd0fbea560ea47829f836b1d790a03a
Daniil <d_tareev@mail.ru>
2023-01-11 22:55:52 +0300

    final

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const advantages = [
+  {
+    img: "../assets/images/advantages/first.svg",
+    title: "Fast <br> Delivery",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+  {
+    img: "../assets/images/advantages/second.svg",
+    title: "Great Customer <br> Service",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+  {
+    img: "../assets/images/advantages/third.svg",
+    title: "Original <br> Plants",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+  {
+    img: "../assets/images/advantages/fourth.svg",
+    title: "Affordable <br> Price",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+];
+
+export const featured = [
+  {
+    img: "../assets/images/featured/first.png",
+    title: "Kaktus Plants",
+    price: "IDR 85.000",
+  },
+  {
+    img: "../assets/images/featured/second.png",
+    title: "Landak Plants",
+    price: "IDR 105.000",
+  },
+  {
+    img: "../assets/images/featured/third.png",
+    title: "Kecubung Plants",
+    price: "IDR 85.000",
+  },
+  {
+    img: "../assets/images/featured/fourth.png",
+    title: "Kecubung Plants",
+    price: "IDR 85.000",
+  },
+  {
+    img: "../assets/images/featured/fifth.png",
+    title: "Kecubung Plants",
+    price: "IDR 85.000",
+  },
+];
